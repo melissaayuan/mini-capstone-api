b7291cd22fc8ef394aa9bf9d7e8b4c6ea01a1c80
Melissa Yuan <mmartchek@gmail.com>
2022-05-03 17:49:17 -0700

    added controller and route

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+end
+
+class Product
+  attr_accessor :name, :price, :image_url, :description
+
+  def initialize(options)
+  @name = options[:name]
+  @price = options[:price]
+  @image_url = options[:image_url]
+  @description = options[:description]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/get_product", controller: "products_controller", action: "product" 
 end
