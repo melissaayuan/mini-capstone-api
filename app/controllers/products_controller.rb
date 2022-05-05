6bc59585e1bf6e3175104f9182465466e66d51fa
Melissa Yuan <mmartchek@gmail.com>
2022-05-04 18:29:52 -0700

    delete function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,12 @@ class ProductsController < ApplicationController
     product.save 
     render json: product.as_json
   end
+
+  def destroy
+     product = Product.find_by(id: params["id"])
+     product.destroy
+     render json: {message: "Product successfully destory"}
+  end
 end
 
   
