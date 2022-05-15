6b97ff494ecbd0a8995bfb8b9d609388c5da8c60
Melissa Yuan <mmartchek@gmail.com>
2022-05-15 12:14:02 -0700

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,33 @@
 class OrdersController < ApplicationController
 
   def index
-    orders = Order.all
-    render json: orders.as_json
+    @orders = Order.all
+    orders = Order.where(user_id: current_user.id)
+    render template: "orders/index"
   end
   
   def show
-    order = Order.find_by(id: params[:id])
-    render json: order.as_json
+    @order = Order.find_by(id: params[:id], user_id: current_user.id)
+    render template: "orders/show"
   end
 
   
   def create
-    order = Order.new(
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:tax],
-      total: params[:total]
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
-    order.save 
-    render json: order.as_json
+    @order.save 
+    render template: "orders/show"
 
   end
 
